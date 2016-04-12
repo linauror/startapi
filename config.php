7bf338bf688816ad250ca2831fbfd3c650e62e13
linauror <linauror@gmail.com>
2016-04-13 01:29:07 +0800

    update readme
    update readme

--- config.php
+++ config.php
@@ -7,5 +7,5 @@ return array(
 	'test_domain' => 'http://test.startapi.cn/',
 	'pro_domain' => 'http://www.startapi.cn/',
 	'last_update' => '2016-04-10',
-	'desc' => 'StartApi是一款基于php+mysql的简易文档编写工具,通过输入主要数据,快捷生成API文档以供查阅和直接测试',
+	'desc' => 'StartApi是一款基于php+mysql的简易接口文档编写生成工具,通过输入主要数据,快捷生成接口文档以供查阅和直接测试',
 );
